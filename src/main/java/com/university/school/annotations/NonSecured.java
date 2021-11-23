db08f27ec8f9eac0b1e37cbe053a506f89a45e12
Pawel Szkudlarek <pawelszkudlarek@wp.pl>
2021-11-23 21:52:55 +0100

    fix security profile nonsecured

--- src/main/java/com/university/school/annotations/NonSecured.java
+++ src/main/java/com/university/school/annotations/NonSecured.java
@@ -5,11 +5,12 @@ import org.springframework.context.annotation.Profile;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Retention(RUNTIME)
-@Target(TYPE)
+@Target({TYPE,METHOD})
 @Profile("nonsecured")
 public @interface NonSecured {
 }
--- src/main/java/com/university/school/security/NoSecurityConfiguration.java
+++ src/main/java/com/university/school/security/NoSecurityConfiguration.java
@@ -1,14 +1,14 @@
 package com.university.school.security;
 
+import com.university.school.annotations.NonSecured;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Profile("nonsecure")
+@NonSecured
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
